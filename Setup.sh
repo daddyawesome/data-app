e9a37982c2c341eaeb07305e5396eff4c6589d56
Daddy Awesome <sablay296@gmail.com>
2021-01-13 16:12:19 -0500

    getting ready for deployment 3

--- Setup.sh
+++ Setup.sh
@@ -8,7 +8,8 @@ email = \"sir.markds@gmail.com\"\n\
 
 echo "\
 [server]\n\
-headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
 " > ~/.streamlit/config.toml
